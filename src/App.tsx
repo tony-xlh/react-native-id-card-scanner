4fdc52099bdee1fe1bae89fe97e3f5f3d16ff7b4
xulihang <xulihanghai@163.com>
2024-02-26 13:24:48 +0800

    adjust code structure

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import CameraScreen from './screens/CameraScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/screens/CameraScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CameraScreen(){
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Camera Screen</Text>
+    </View>
+  )
+}
