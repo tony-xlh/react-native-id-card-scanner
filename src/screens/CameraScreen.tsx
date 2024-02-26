c168ad35da700ca99feaba03a659e95537704193
xulihang <xulihanghai@163.com>
2024-02-26 14:56:29 +0800

    use react-native-svg to draw the overlay

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Platform } from "react-native";
+import Svg, { Rect } from "react-native-svg";
 import { Camera, useCameraDevice, useCameraFormat } from "react-native-vision-camera";
 
 export default function CameraScreen(){
@@ -10,16 +11,49 @@ export default function CameraScreen(){
     { videoResolution: { width: 1920, height: 1080 } },
     { fps: 30 }
   ])
+  const [cropRegion,setCropRegion] = useState({
+    left: 10,
+    top: 20,
+    width: 80,
+    height: 30
+  });
   const [shouldTake,setShouldTake] = useState(false);
   const [pressed,setPressed] = useState(false);
   const capture = () => {
     setShouldTake(true);
   }
+
+  const adaptCropRegionForIDCard = () => {
+    let size = getFrameSize();
+    let regionWidth = 0.8*size.width;
+    let desiredRegionHeight = regionWidth/(85.6/54);
+    let height = Math.ceil(desiredRegionHeight/size.height*100);
+    let region = {
+      left:10,
+      width:80,
+      top:20,
+      height:height
+    };
+    setCropRegion(region);
+  }
+
+  const getViewBox = () => {
+    const frameSize = getFrameSize();
+    const viewBox = "0 0 "+frameSize.width+" "+frameSize.height;
+    return viewBox;
+  }
+
+  const getFrameSize = ():{width:number,height:number} => {
+    let size = {width:1080,height:1920};
+    return size;
+  }
+
   useEffect(() => {
     (async () => {
       const status = await Camera.requestCameraPermission();
       setHasPermission(status === 'granted');
       setIsActive(true);
+      adaptCropRegionForIDCard();
     })();
   }, []);
   
@@ -35,6 +69,17 @@ export default function CameraScreen(){
           format={format}
           pixelFormat='yuv'
         />
+         <Svg preserveAspectRatio={(Platform.OS == 'ios') ? '':'xMidYMid slice'} style={StyleSheet.absoluteFill} viewBox={getViewBox()}>
+          <Rect 
+            x={cropRegion.left/100*getFrameSize().width}
+            y={cropRegion.top/100*getFrameSize().height}
+            width={cropRegion.width/100*getFrameSize().width}
+            height={cropRegion.height/100*getFrameSize().height}
+            strokeWidth="2"
+            stroke="red"
+            fillOpacity={0.0}
+          />
+        </Svg>
       </>
       )}
       <View style={[styles.bottomBar]}>
