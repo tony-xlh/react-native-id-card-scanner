852ab3a747532405eb1d19b615544d191e945f83
xulihang <xulihanghai@163.com>
2024-02-27 10:16:28 +0800

    add a text button in the header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import CameraScreen from './screens/CameraScreen';
 import CardScreen from './screens/CardScreen';
+import { Button, Pressable, StyleSheet, Text } from 'react-native';
+import { TextButton } from './components/TextButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +15,13 @@ function App(): React.JSX.Element {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Camera" component={CameraScreen} />
-        <Stack.Screen name="Card" component={CardScreen} />
+        <Stack.Screen name="Card" component={CardScreen} 
+         options={({ navigation, route }) => ({
+          // Add a placeholder button without the `onPress` to avoid flicker
+          headerRight: () => (
+            <TextButton title="Save"></TextButton>
+          ),
+        })}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
