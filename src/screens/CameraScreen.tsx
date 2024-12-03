0e503ddb136e07c9e3ccd45cb2528c3c4be4eccb
xulihang <xulihanghai@163.com>
2024-12-03 14:33:24 +0800

    use Dynamsoft Document Normalizer to crop the image

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Pressable, Platform } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { View, Text, StyleSheet, Pressable, Platform, Switch } from "react-native";
 import Svg, { Rect } from "react-native-svg";
 import { Camera, useCameraDevice, useCameraFormat, useFrameProcessor } from "react-native-vision-camera";
 import { useSharedValue, Worklets } from "react-native-worklets-core";
 import * as Cropper from "vision-camera-cropper";
 import { CropRegion } from "vision-camera-cropper";
+import * as DDN from "vision-camera-dynamsoft-document-normalizer";
 
 export interface CameraScreenProps {
   route:any;
@@ -13,13 +14,20 @@ export interface CameraScreenProps {
 
 export default function CameraScreen(props:CameraScreenProps){
   console.log(Cropper);
+
   const [hasPermission, setHasPermission] = useState(false);
+  const [crop, setCrop] = useState(false);
+  const useCrop = useRef(false);
   const [isActive,setIsActive] = useState(true);
   const device = useCameraDevice("back");
   const format = useCameraFormat(device, [
     { videoResolution: { width: 1920, height: 1080 } },
     { fps: 30 }
   ])
+  const toggleSwitch = () => {
+    useCrop.current = !crop;
+    setCrop(previousState => !previousState);
+  };
   const [cropRegion,setCropRegion] = useState({
     left: 10,
     top: 20,
@@ -59,8 +67,22 @@ export default function CameraScreen(props:CameraScreenProps){
     return size;
   }
 
-  const onCaptured = (base64:string) => {
+  const onCaptured = async (base64:string) => {
     setIsActive(false);
+    console.log("onCaptured");
+    console.log(useCrop.current);
+    if (useCrop.current) {
+      console.log(base64);
+      const detectedQuads = await DDN.detectBase64(base64);
+      console.log(detectedQuads);
+      if (detectedQuads.length>0) {
+        const normalized = await DDN.normalizeBase64(base64,detectedQuads[0].location,{includeNormalizationResultAsBase64:true});
+        console.log(normalized);
+        if (normalized.imageBase64) {
+          base64 = normalized.imageBase64;
+        }
+      }
+    }
     if (props) {
       if (props.navigation) {
         props.navigation.navigate({
@@ -125,9 +147,19 @@ export default function CameraScreen(props:CameraScreenProps){
           onPressOut={()=>{setPressed(false)}}
           onPress={()=>{capture()}}>
           <View style={styles.outerCircle}>
-          <View style={[styles.innerCircle, pressed ? styles.circlePressed:null]}></View>
+            <View style={[styles.innerCircle, pressed ? styles.circlePressed:null]}></View>
           </View>
         </Pressable>
+      </View>
+      <View style={[styles.control]}>
+        <Text>Crop:</Text>
+        <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          thumbColor={crop ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={crop}
+        />
       </View>
     </View>
   )
@@ -142,6 +174,15 @@ const styles = StyleSheet.create({
     height: 60,
     flexDirection:"row",
     justifyContent:"center",
+    backgroundColor:'rgba(0, 0, 0, 0.2)',
+  },
+  control:{
+    flexDirection:"row",
+    position: 'absolute',
+    bottom: 0,
+    height: 60,
+    alignSelf:"flex-start",
+    alignItems: 'center',
   },
   outerCircle: {
     width: 60,
